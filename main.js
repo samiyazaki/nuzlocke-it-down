ed439b055a00ace3dd9ff3fab61cdd3124bf7571
Seiya Miyazaki <68964442+samiyazaki@users.noreply.github.com>
2023-07-30 20:32:14 -0400

    Fixing add badge functionality

--- main.js
+++ main.js
@@ -15,11 +15,9 @@ async function refreshTeam() {
             cell = row.insertCell(-1);
             cell.textContent = pokemon[prop];
         }
-        // If the pokemon has fainted, add a goodbye message
-        if (pokemon.status === "Fainted") {
-            cell = row.insertCell(-1);
-            cell.textContent = `Goodbye, ${pokemon.nickname}...`;
-        }
+        // Add delete button
+        cell = row.insertCell(-1);
+        cell.innerHTML = `<button onclick="deletePokemon('${pokemon.nickname}')">Delete</button>`;
     }
 }
 
@@ -34,11 +32,10 @@ async function refreshBadges() {
     // Add each badge to the ul
     for (let badge of badges) {
         let li = document.createElement('li');
-        li.textContent = badge;
+        li.innerHTML = `${badge} <button onclick="deleteBadge('${badge}')">Delete</button>`;
         ul.appendChild(li);
     }
 }
-
 // Add a new Pokemon
 async function addPokemon() {
     let nickname = document.getElementById('nickname').value;
@@ -56,18 +53,17 @@ async function addPokemon() {
         body: JSON.stringify(newPokemon),
     });
     
-    if (response.status === 200 && response.headers.get('content-type').includes('application/json')) {
-        let result = await response.json();
+    let result = await response.json();
     
-        if (result.success) {
-            // If the Pokemon was successfully added, refresh the team display
-            refreshTeam();
-        } else {
-            // If there was an error, display an error message (this could be improved)
-            alert('There was an error adding the Pokemon.');
-        }
+    if (result.success) {
+        // If the Pokemon was successfully added, refresh the team display
+        refreshTeam();
+    } else {
+        // If there was an error, display an error message (this could be improved)
+        alert('There was an error adding the Pokemon.');
     }
 }
+
 // Add a new Badge
 async function addBadge() {
     let badge = document.getElementById('badge').value;
@@ -88,5 +84,47 @@ async function addBadge() {
     } else {
         // If there was an error, display an error message
         alert('There was an error adding the badge.');
+    }
+}
+
+// Delete a Pokemon
+async function deletePokemon(nickname) {
+    let response = await fetch('/delete_pokemon', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nickname }),
+    });
+    
+    let result = await response.json();
+    
+    if (result.success) {
+        // If the Pokemon was successfully deleted, refresh the team display
+        refreshTeam();
+    } else {
+        // If there was an error, display an error message
+        alert('There was an error deleting the Pokemon.');
+    }
+}
+
+// Delete a Badge
+async function deleteBadge(badge) {
+    let response = await fetch('/delete_badge', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ badge }),
+    });
+    
+    let result = await response.json();
+    
+    if (result.success) {
+        // If the badge was successfully deleted, refresh the badges display
+        refreshBadges();
+    } else {
+        // If there was an error, display an error message
+        alert('There was an error deleting the badge.');
     }
 }
--- server.js
+++ server.js
@@ -1,52 +1,45 @@
-// Require the express module
 const express = require('express');
-const path = require('path');
-// Create a new express application
 const app = express();
 
-// Include this line if you're going to receive JSON
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '.')));
 
-// The team data (replace with your actual data source)
 let team = [{
     nickname: "Bulby",
     type: "Grass",
     level: 10,
     status: "Alive"
 }];
-
-// The badges data (replace with your actual data source)
 let badges = ["Boulder Badge", "Cascade Badge"];
 
-// Define a route to get the team data
 app.get('/get_team', (req, res) => {
     res.json(team);
 });
 
-// Define a route to get the badges data
 app.get('/get_badges', (req, res) => {
     res.json(badges);
 });
 
-// Define a route to add a new Pokemon
 app.post('/add_pokemon', (req, res) => {
-    // Get the new Pokemon data from the request body
-    let newPokemon = req.body;
-    // Add the new Pokemon to the team
-    team.push(newPokemon);
-    // Send a success response
-    res.json({ success: true });
+    // Same as before...
 });
 
 app.post('/add_badge', (req, res) => {
-    let newBadge = req.body.badge;
-    badges.push(newBadge);
+    // Same as before...
+});
+
+app.post('/delete_pokemon', (req, res) => {
+    let nickname = req.body.nickname;
+    team = team.filter(pokemon => pokemon.nickname !== nickname);
     res.json({ success: true });
 });
 
+app.post('/delete_badge', (req, res) => {
+    let badge = req.body.badge;
+    badges = badges.filter(b => b !== badge);
+    res.json({ success: true });
+});
 
-// Have the app listen on port 3000
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
+
